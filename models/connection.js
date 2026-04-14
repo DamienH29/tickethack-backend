807259e9f04208c9aa02ed64fe2fa162fa6de443
DamienH <d.hamon29@laposte.net>
2026-04-14 10:14:03 +0200

    first commit

---
+++ models/connection.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.CONNECTION_STRING, {
+    connectTimeoutMS: 2000,
+  })
+  .then(() => console.log("✅ Connecté à MongoDB"))
+  .catch((err) => console.error("❌ Erreur MongoDB:", err));
